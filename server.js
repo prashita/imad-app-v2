d73688a6c323acc30b81beee07437baf08594418
prashita <prashinomad@gmail.com>
2017-02-16 13:15:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function(req, res) {
     var articleName=req.params.articleName;
-     res.send(createTemplate(articleOne));
+     res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two', function(req, res) {
