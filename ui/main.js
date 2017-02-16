8477b37c7460a34fddee6dd92b0cf18b00eea432
prashita <prashinomad@gmail.com>
2017-02-16 20:04:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ element.innerHTML = 'New value';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLef + 1;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft ='px';
 }
 img.onclick = function () {
